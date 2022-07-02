01718bbb3cc69f990f38e5d80a7b88893641e935
Moeid Heidari <moeidheidari@comfortel.pro>
2022-07-02 14:51:21 +0300

    Initialize tls

---
+++ .github/workflows/scripts/run-tests-enterprise-search.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+
+source "$(dirname ${BASH_SOURCE[0]})/lib/testing.sh"
+
+
+cid_es="$(container_id elasticsearch)"
+cid_en="$(container_id enterprise-search)"
+
+ip_es="$(service_ip elasticsearch)"
+ip_en="$(service_ip enterprise-search)"
+
+es_ca_cert="$(realpath $(dirname ${BASH_SOURCE[0]})/../../../tls/kibana/elasticsearch-ca.pem)"
+
+log 'Waiting for readiness of Elasticsearch'
+poll_ready "$cid_es" 'https://elasticsearch:9200/' --resolve "elasticsearch:9200:${ip_es}" --cacert "$es_ca_cert" -u 'elastic:testpasswd'
+
+log 'Waiting for readiness of Enterprise Search'
+poll_ready "$cid_en" "http://${ip_en}:3002/api/ent/v1/internal/health" -u 'elastic:testpasswd'
+
+log 'Ensuring that App Search API keys were created in Elasticsearch'
+response="$(curl 'https://elasticsearch:9200/.ent-search-actastic-app_search_api_tokens_v3/_search?q=*:*&pretty' -s --resolve "elasticsearch:9200:${ip_es}" --cacert "$es_ca_cert" -u elastic:testpasswd)"
+echo "$response"
+declare -i count
+count="$(jq -rn --argjson data "${response}" '$data.hits.total.value')"
+if (( count != 2)); then
+	echo "Expected search and private keys, got ${count} result(s)"
+	exit 1
+fi
---
+++ .github/workflows/scripts/run-tests-logspout.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+
+source "$(dirname ${BASH_SOURCE[0]})/lib/testing.sh"
+
+
+cid_es="$(container_id elasticsearch)"
+cid_ls="$(container_id logstash)"
+cid_lsp="$(container_id logspout)"
+
+ip_es="$(service_ip elasticsearch)"
+ip_ls="$(service_ip logstash)"
+ip_lsp="$(service_ip logspout)"
+
+es_ca_cert="$(realpath $(dirname ${BASH_SOURCE[0]})/../../../tls/kibana/elasticsearch-ca.pem)"
+
+log 'Waiting for readiness of Elasticsearch'
+poll_ready "$cid_es" 'https://elasticsearch:9200/' --resolve "elasticsearch:9200:${ip_es}" --cacert "$es_ca_cert" -u 'elastic:testpasswd'
+
+log 'Waiting for readiness of Logstash'
+poll_ready "$cid_ls" "http://${ip_ls}:9600/_node/pipelines/main?pretty"
+
+log 'Waiting for readiness of Logspout'
+poll_ready "$cid_lsp" "http://${ip_lsp}/health"
+
+# When Logspout starts, it prints the following log line:
+#   2021/01/07 16:14:52 # logspout v3.2.13-custom by gliderlabs
+#
+# which we expect to find by querying:
+#   docker.image:"docker-elk_logspout" AND message:"logspout gliderlabs"~3
+#
+log 'Searching a log entry forwarded by Logspout'
+
+declare response
+declare -i count
+
+declare -i was_retried=0
+
+# retry for max 60s (30*2s)
+for _ in $(seq 1 30); do
+	response="$(curl "https://elasticsearch:9200/logs-generic-default/_search?q=docker.image:%22docker-elk_logspout%22%20AND%20message:%22logspout%20gliderlabs%22~3&pretty" -s --resolve "elasticsearch:9200:${ip_es}" --cacert "$es_ca_cert" -u elastic:testpasswd)"
+
+	set +u  # prevent "unbound variable" if assigned value is not an integer
+	count="$(jq -rn --argjson data "${response}" '$data.hits.total.value')"
+	set -u
+
+	if (( count > 0 )); then
+		break
+	fi
+
+	was_retried=1
+	echo -n 'x' >&2
+	sleep 2
+done
+if ((was_retried)); then
+	# flush stderr, important in non-interactive environments (CI)
+	echo >&2
+fi
+
+echo "$response"
+# Logspout may restart if Logstash isn't ready yet, so we tolerate multiple
+# results
+if (( count == 0 )); then
+	echo 'Expected at least 1 document'
+	exit 1
+fi
